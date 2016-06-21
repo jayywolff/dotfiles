a5aaa468b279acb61b473a5dd4f9d41fecbb1a44
Jayy Wolff <jaywolff16@gmail.com>
2016-06-21 14:43:36 -0400

    added script to toggle gaps

---
+++ scripts/i3_gaps.sh
@@ -0,0 +1,18 @@
+#! /bin/env bash
+
+# A basic script for toggling on and off i3-gaps
+
+# simply add this line to your i3-config file to use
+# bindsym $mod+g exec bash ~/dotfiles/scripts/i3_gaps.sh
+
+tmp_file="/tmp/.i3-gaps"
+
+if [ ! -e "$tmp_file" ]; then
+    pkill compton
+    i3-msg gaps inner all set 0
+    touch $tmp_file
+else
+    compton -bc
+    i3-msg gaps inner all set 25
+    rm $tmp_file
+fi
