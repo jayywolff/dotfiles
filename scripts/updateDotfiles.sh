7e32630b5480cc36372a6ef81d07abc16d876761
Jayy Wolff <jaywolff16@gmail.com>
2016-08-16 02:55:39 -0400

    termite included

--- scripts/updateDotfiles.sh
+++ scripts/updateDotfiles.sh
@@ -5,6 +5,7 @@ dotdir="$HOME/dotfiles"
 ln -sf $dotdir/.i3/ $HOME/.i3 
 ln -sf $dotdir/.config/ranger/rc.conf $HOME/.config/ranger/rc.conf
 ln -sf $dotdir/.config/htop/htoprc $HOME/.config/htop/htoprc
+ln -sf $dotdir/.config/termite/config $HOME/.config/termite/config
 ln -sf $dotdir/.config/gtk-3.0/settings.ini $HOME/.config/gtk-3.0/settings.ini
 ln -sf $dotdir/.gtkrc-2.0 $HOME/.gtkrc-2.0
 ln -sf $dotdir/.zshrc $HOME/.zshrc
