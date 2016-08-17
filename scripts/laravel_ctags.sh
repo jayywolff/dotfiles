3e730480b89f331e7bef453e7bb112111f08e9a5
Jayy Wolff <jaywolff16@gmail.com>
2016-08-17 13:59:47 -0400

    add traits to laravel ctags

--- scripts/laravel_ctags.sh
+++ scripts/laravel_ctags.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #This script generates custom ctags for laravel projects (could be optimized a bit better)
 cd $1
-ctags -R --fields=+aimlS --languages=php --exclude="vendor" --exclude="node_modules" --exclude=".git*" --exclude=".env" .
+ctags -R --fields=+aimlS --languages=php --exclude="vendor|node_modules|.git*|.env" --regex-php=/^[ \t]*trait[ \t]+([a-z0_9_]+)/\1/t,traits/i .
 ctags -R --fields=+aimlS --languages=php --append vendor/laravel 
 grep -v "/\^use" tags > tags_temp; mv tags_temp tags
 grep -v "/\^namespace" tags > tags_temp; mv tags_temp tags
