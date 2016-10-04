a00a614d209cba3e0aa774c77a4da449857b2df4
Jayy Wolff <jaywolff16@gmail.com>
2016-10-04 09:50:05 -0400

    updated config for crappy work monitor

--- scripts/configure_xrandr.sh
+++ scripts/configure_xrandr.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-#This script is basically a toggle switch between my most used setups
-#built-in display only & built-in + external to the right
-#Todo - also setup clamp-shell mode
+# This script is basically a toggle switch between my most used monitor setups
 
 LAPTOP="eDP-1"
 HDMI="HDMI-1"
 
 if (xrandr | grep "$LAPTOP connected" && xrandr | grep "$HDMI connected"); then
     xrandr --output $HDMI --auto --right-of $LAPTOP
+    # Force work monitor into 1920x1080 mode
+    if (xrandr | grep "$HDMI connected 1440x900+1920+0"); then
+        xrandr --output $HDMI --mode 1920x1080
+    fi
 elif (xrandr | grep "$HDMI disconnected" && xrandr | grep "$LAPTOP connected"); then
 	xrandr --output $HDMI --off
 	xrandr --output $LAPTOP --primary --auto
-else #reset and default to only laptop display, manually config other setups
+else # Reset and default to only laptop display, manually config other setups
 	xrandr --output $HDMI --off
 	xrandr --output $LAPTOP --off
 	xrandr --output $LAPTOP --primary --auto
 fi
 
+i3-msg restart
