ef12fa512a5332bfafcb1d541293649f28181308
Jayy Wolff <jaywolff16@gmail.com>
2017-05-09 14:26:39 -0400

    spacing

--- scripts/i3blocks/battery.sh
+++ scripts/i3blocks/battery.sh
@@ -3,17 +3,17 @@ batinf='acpi -b'
 perc=$(acpi -b | awk '{print $4;}' | sed 's/%//g' | sed 's/,//g')
 charging=$(acpi -b | grep -c Discharging)
 if [ ! $charging -eq 1 ]
-then echo "  $perc%"
+then echo " $perc%"
 elif (( $perc > '75' ))
-then echo "  $perc%"
+then echo " $perc%"
 elif (( $perc > '50' ))
-then echo "  $perc%"
+then echo " $perc%"
 elif (( $perc > '25' ))
-then echo "  $perc%"
+then echo " $perc%"
 elif  (( $perc > '10' ))
-then echo "  $perc%"
+then echo " $perc%"
 else
-echo "  $perc%"
+echo " $perc%"
 fi
 
 case $BLOCK_BUTTON in
