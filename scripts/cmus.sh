d67bf937ddfcd98df4675ad220702438222cddb7
Jayy Wolff <jaywolff16@gmail.com>
2017-05-09 14:12:15 -0400

    created cmus script fot tmux line

---
+++ scripts/cmus.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+song="$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- | sed '3d' | sed '1!G;h;$!d' | sed 'N;s/\n/ - /')"
+
+song_details="$(cmus-remote -Q | grep tag)"
+
+if ! $song_details; then
+    artist="$(cmus-remote -Q | grep 'tag artist' | cut -d ' ' -f 3)"
+    title="$(cmus-remote -Q | grep 'tag title' | cut -d ' ' -f3-)"
+    echo "  $artist - $title"
+fi
