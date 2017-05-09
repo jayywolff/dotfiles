3a7767d17f7a98a5bfcf8bb7ea1bb5a68a869461
Jayy Wolff <jaywolff16@gmail.com>
2017-05-09 14:35:29 -0400

    fix for artist name

--- scripts/cmus.sh
+++ scripts/cmus.sh
@@ -4,7 +4,7 @@ song="$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- | sed
 song_details="$(cmus-remote -Q | grep tag)"
 
 if ! $song_details; then
-    artist="$(cmus-remote -Q | grep 'tag artist' | cut -d ' ' -f 3)"
+    artist="$(cmus-remote -Q | grep 'tag artist' | cut -d ' ' -f3-)"
     title="$(cmus-remote -Q | grep 'tag title' | cut -d ' ' -f3-)"
     echo "  $artist - $title"
 fi
