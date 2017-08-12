7989f9299b2538a87857156c75a9215c78e5eab6
Jayy Wolff <jaywolff16@gmail.com>
2017-08-11 22:25:06 -0400

    update i3blocks

--- scripts/i3blocks/battery.sh
+++ scripts/i3blocks/battery.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 batinf='acpi -b'
-perc=$(acpi -b | awk '{print $4;}' | sed 's/%//g' | sed 's/,//g')
-charging=$(acpi -b | grep -c Discharging)
+perc=$(acpi -b | awk '{print $4;}' | sed 's/%//g' | sed 's/,//g' | head -n1)
+charging=$(acpi -b | head -n1 | grep -c Discharging)
 if [ ! $charging -eq 1 ]
 then echo " $perc%"
-elif (( $perc > '75' ))
+elif [ $perc -gt 75 ]
 then echo " $perc%"
-elif (( $perc > '50' ))
+elif [ $perc -gt 50 ]
 then echo " $perc%"
-elif (( $perc > '25' ))
+elif [ $perc -gt 25 ]
 then echo " $perc%"
-elif  (( $perc > '10' ))
+elif  [ $perc -gt 10 ]
 then echo " $perc%"
 else
 echo " $perc%"
