63e1d28967ab74736f3962e5ef70aa09050fc7e5
Jay <jaywolff16@gmail.com>
2020-01-02 10:42:48 -0500

    update coc-extensions on sys update

--- scripts/system_update.sh
+++ scripts/system_update.sh
@@ -5,8 +5,9 @@ echo 'Running oh-my-zsh updates'
 sh $ZSH/tools/upgrade.sh
 echo 'Running tmux plugin updates'
 sh ~/.tmux/plugins/tpm/bin/update_plugins all
-echo 'Running coc-vim updates'
-cd ~/.vim/bundle/coc.nvim && git pull origin release && cd ~/
 echo 'Running vim plugin updates'
 vim -i NONE -c VundleUpdate -c VundleChangelog -c 'w ~/.vim/bundle/changelog.md' -c quitall
 cat ~/.vim/bundle/changelog.md
+echo 'Running coc-vim updates'
+cd ~/.vim/bundle/coc.nvim && git pull origin release && cd ~/
+vim -c 'CocUpdateSync|q'
