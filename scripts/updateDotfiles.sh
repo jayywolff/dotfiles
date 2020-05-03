c301c8cbcf1ffc088d449fb0f6b57c9cb7eabdf9
Jay <jaywolff16@gmail.com>
2020-05-03 18:59:41 -0400

    updated install script

--- scripts/updateDotfiles.sh
+++ scripts/updateDotfiles.sh
@@ -5,6 +5,8 @@ mkdir -p $HOME/.config/ranger && ln -sf $dotdir/.config/ranger/rc.conf $HOME/.co
 mkdir -p $HOME/.config/htop && ln -sf $dotdir/.config/htop/htoprc $HOME/.config/htop/htoprc
 mkdir -p $HOME/.config/termite && ln -sf $dotdir/.config/termite/config $HOME/.config/termite/config
 rm -rf $HOME/.config/i3 && ln -sf $dotdir/.config/i3 $HOME/.config/i3
+ln -sf $dotdir/.config/polybar $HOME/.config/polybar
+ln -sf $dotdir/.config/fontconfig $HOME/.config/fontconfig
 ln -sf $dotdir/.config/feh $HOME/.config/feh
 ln -sf $dotdir/.config/mupen64plus $HOME/.config/mupen64plus
 ln -sf $dotdir/.config/picom.conf $HOME/.config/picom.conf
@@ -18,7 +20,9 @@ ln -sf $dotdir/.vim/ $HOME/.vim
 ln -sf $dotdir/.vimrc $HOME/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+# there may be some un-automated setup required for coc-vim
 
+ln -sf $dotdir/.vim/ $HOME/.vim
 # setup tmux
 ln -sf $dotdir/.tmux.conf $HOME/.tmux.conf 
 mkdir -p $HOME/.tmux/plugins
@@ -26,10 +30,14 @@ if "test ! -d ~/.tmux/plugins/tpm" "run 'git clone https://github.com/tmux-plugi
 tmux source $HOME/.tmux.conf
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-# lightdm setup
+# lightdm setup (manually)
 #sudo mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.old.conf
-#sudo ln -f $dotdir/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-#sudo chown root /etc/lightdm/lightdm-gtk-greeter.conf
+#sudo cp $dotdir/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+#sudo chown root:root /etc/lightdm/lightdm-gtk-greeter.conf
+#sudo vim /etc/lightdm/lightdm.conf 
+#change `#display-setup-script=` to `display-setup-script=/usr/share/setup-monitors.sh`
+#sudo vim /usr/share/setup-monitors.sh
+#make this a bash script that sets up xrandr
 
 #setup oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -37,3 +45,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # misc directories to be created
 mkdir -p $HOME/Dev
 mkdir -p $HOME/Pictures/screenshots
+
+# other notes
+# check out https://wiki.archlinux.org/index.php/Font_configuration
