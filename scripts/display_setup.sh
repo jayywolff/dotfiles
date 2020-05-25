12a49f92ab68528f095e89cc47344cf0eaee6555
Jay <jaywolff16@gmail.com>
2020-05-25 15:01:03 -0400

    Simplify display setup

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
-EDP1="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep eDP-1)"
 HDMI1="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep HDMI-1)"
 HDMI2="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep HDMI-2)"
+HDMI3="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep HDMI-3)"
 
-if [[ ! -n $HDMI2 ]] && [[ ! -n $HDMI1 ]] # Single monitor setup
+pkill picom
+if [[ ! -n $HDMI1 ]] && [[ ! -n $HDMI2 ]] && [[ ! -n $HDMI3 ]] # Single monitor setup
 then
-    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --off --output HDMI-2 --off
-elif [[ -n $HDMI2 ]] && [[ ! -n $HDMI1 ]] # Dual monitor setup with DVI
+    xrandr --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output eDP-1 --primary --mode 1920x1200
+elif [[ -n $HDMI1 ]] # Dual monitor setup with HDMI1 and EDP1
 then
-    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-2 --mode 1920x1080 --right-of eDP-1
-elif [[ ! -n $HDMI2 ]] && [[ -n $HDMI1 ]] # Dual monitor setup with HDMI
+    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --mode 1920x1080 --right-of eDP-1 --output HDMI-2 --off --output HDMI-3 --off
+elif [[ -n $HDMI2 ]] # Dual monitor setup with HDMI2 and EDP1
 then
-    xrandr --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --mode 1920x1080 --left-of eDP-1
-elif [[ -n $HDMI2 ]] && [[ -n $HDMI1 ]] # Triple monitor setup
+    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-2 --mode 1920x1080 --right-of eDP-1 --output HDMI-1 --off --output HDMI-3 --off
+elif [[ -n $HDMI3 ]] # Dual monitor setup with HDMI3 and EDP1
 then
-    # only using 2 external monitors
-    xrandr --output eDP-1 --off --output HDMI-2 --mode 1920x1080 --right-of HDMI-1 --output HDMI-1 --primary --mode 1920x1080
-
-    # 2 external monitors & laptop display
-    #xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-2 --mode 1920x1080 --right-of eDP-1 --output HDMI-1 --mode 1920x1080 --left-of eDP-1
+    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-3 --mode 1920x1080 --right-of eDP-1 --output HDMI-1 --off --output HDMI-2 --off
 else # Fail-safe
-    xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --off --output HDMI-2 --off
+    xrandr --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output eDP-1 --primary --mode 1920x1200
 fi
 
-sleep 0.2
-feh --bg-scale ~/Pictures/wallpaper.*
-sleep 0.2
-pkill picom
+sleep 1
 picom --daemon --config ~/dotfiles/.config/picom.conf
+sleep 0.5
+i3-msg restart
