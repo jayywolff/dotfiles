f07ffe33d4a2f2fd8949748d9f9a5a78ee67bed4
Jay <jaywolff16@gmail.com>
2020-07-01 11:33:36 -0400

    another display config

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -7,6 +7,9 @@ pkill picom
 if [[ ! -n $HDMI1 ]] && [[ ! -n $HDMI2 ]] && [[ ! -n $HDMI3 ]] # Single monitor setup
 then
     xrandr --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output eDP-1 --primary --mode 1920x1200
+elif [[ -n $HDMI1 ]] && [[ -n $HDMI2 ]] && [[ ! -n $HDMI3 ]] # Dual monitor setup with HDMI1 and HDMI2
+then
+    xrandr --output HDMI-1 --mode 1920x1080 --primary --output HDMI-2 --mode 1920x1080 --right-of HDMI-1 --output eDP-1 --off
 elif [[ -n $HDMI1 ]] # Dual monitor setup with HDMI1 and EDP1
 then
     xrandr --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --mode 1920x1080 --right-of eDP-1 --output HDMI-2 --off --output HDMI-3 --off
