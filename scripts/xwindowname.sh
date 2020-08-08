9fc965e34a1a73f59b95b42a0df6df84c752d73c
Jayy Wolff <jaywolff16@gmail.com>
2020-08-08 14:47:34 -0400

    fix xwindowname script when workspace has no window

--- scripts/xwindowname.sh
+++ scripts/xwindowname.sh
@@ -1,12 +1,17 @@
 #! /bin/zsh
 
-WIN=$(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}"))
-WINDOW_CLASS=$(echo $WIN | awk '/WM_CLASS/{print $4}' | tr --delete \")
-WINDOW_NAME=$(echo $WIN | awk '/WM_NAME\(STRING\)/{print $3}')
-
-if [[ "$WINDOW_CLASS" == "Termite" ]] && [[ $WINDOW_NAME =~ tmux ]]
+if [[ $(xdotool getwindowfocus getwindowname | cut -c1-40) == "i3" ]]
 then
-  echo $(tmux list-windows | grep '(active)' | awk '{print $2}' | sed 's/\*//')
+  echo "i3"
 else
-  echo $WINDOW_CLASS
+  WIN=$(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}")) || echo "error"
+  WINDOW_CLASS=$(echo $WIN | awk '/WM_CLASS/{print $4}' | tr --delete \")
+  WINDOW_NAME=$(echo $WIN | awk '/WM_NAME\(STRING\)/{print $3}')
+
+  if [[ $WINDOW_CLASS == "Termite" ]] && [[ $WINDOW_NAME =~ "tmux" ]]
+  then
+    echo $(tmux list-windows | grep '(active)' | awk '{print $2}' | sed 's/\*//')
+  else
+    echo $WINDOW_CLASS
+  fi
 fi
