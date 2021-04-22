3feb61a84d1f15be116240d360b7ebd78aa59d42
Jay Wolff <jaywolff16@gmail.com>
2021-04-21 21:44:45 -0400

    fix display setup when undocking

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -5,13 +5,13 @@ DP22="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep DP2-2)"
 
 if [[ ! -n $DP21 ]] && [[ ! -n $DP22 ]] # Single monitor setup
 then
-    xrandr --output eDP1 --primary --mode 1920x1080 --output DP2-1 --off --output DP2-2 --off
+    xrandr --output eDP1 --mode 1920x1080 --primary  --output DP2-1 --off --output DP2-2 --off
 elif [[ -n $DP21 ]] && [[ -n $DP22 ]] # Dual monitor dock setup with HDMI and Display port
 then
     xrandr --output DP2-1 --mode 1920x1080 --primary
     xrandr --output DP2-1 --mode 1920x1080 --primary --output DP2-2 --mode 1920x1080 --right-of DP2-1 --output eDP1 --off
 else # Fail-safe
-    xrandr --output eDP1 --primary --mode 1920x1080 --output DP2-1 --off --output DP2-2 --off
+    xrandr --output eDP1 --mode 1920x1080 --primary  --output DP2-1 --off --output DP2-2 --off
 fi
 
 sleep 0.2
