a4ce84eb8a36536fe171c21a29f066f045ee709d
Jay Wolff <jaywolff16@gmail.com>
2022-11-22 16:18:59 -0500

    display setup fixes

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -5,14 +5,16 @@ RIGHT="$(xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep DP2-3)"
 
 if [[ ! -n $LEFT ]] && [[ ! -n $RIGHT ]] # Single monitor setup
 then
-    xrandr --output eDP1 --mode 1920x1080 --primary 
     xrandr --output DP2-1 --off 
+    xrandr --output DP2-2 --off
     xrandr --output DP2-3 --off
+    xrandr --output eDP1 --mode 1920x1080 --primary
 elif [[ -n $LEFT ]] && [[ -n $RIGHT ]] # Dual monitor dock setup with HDMI and Display port
 then
+    xrandr --output eDP1 --off
+    xrandr --output DP2-2 --off
     xrandr --output DP2-1 --mode 1920x1080 --primary 
     xrandr --output DP2-3 --mode 1920x1080 --right-of DP2-1
-    xrandr --output eDP1 --off
 else # Fail-safe
     xrandr --output eDP1 --mode 1920x1080 --primary --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off
 fi
